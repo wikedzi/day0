31d3d429ffecfe0ef42c754c8faf475f31f4c65f
wikedzi <twikedzi@gmail.com>
2017-02-06 21:56:41 +0300

    Core code for the prime number program
    This file implements the core functionality of the Prime function.... The code defines an empty list of prime numbers and loops through a range of numbers between a given interval to determine which are primes and appends them to the list

---
+++ primes.py
@@ -0,0 +1,11 @@
+def primeNumbers(start,end):
+    primes = []
+    for i in range(start, end+1):
+        if i==2:
+            primes.append(2)
+            continue # continue to the next i
+        for j in range(2, i):
+            if i % j == 0:break #break execution since we have found a factor
+        else:
+            primes.append(i)
+    return primes
