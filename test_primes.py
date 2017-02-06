a2a96231220dbcfe4b532955b7a504caf25f2b17
wikedzi <twikedzi@gmail.com>
2017-02-06 22:59:26 +0300

    Additing more test cases
    Two more test cases are added in this code

--- test_primes.py
+++ test_primes.py
@@ -2,9 +2,15 @@ import unittest
 from primes import primeNumbers
 
 class PrimeFunction(unittest.TestCase):
-
+    
     def test_prime_numbers(self):
         self.assertEqual(primeNumbers(2,10), [2,3,5,7])
+    
+    def test_is_greater_than_zero(self):
+        self.assertTrue(primeNumbers(2,10)!=102)
+
+    def test_start_is_greater_than_end(self):
+        self.assertTrue(primeNumbers(2,10)!=101)
 
 if __name__ == '__main__':
     unittest.main()
